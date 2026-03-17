5950d7cd6c98e4f3a34466233e921deea3b72bcc
smixq <118548569+smixq@users.noreply.github.com>
2026-03-17 21:58:49 +0300

    20

---
+++ Easy/20.go
@@ -0,0 +1,20 @@
+func isValid(s string) bool {
+	var stacBrack []rune
+
+	for _, brack := range s {
+		n := len(stacBrack)
+		if brack == '(' || brack == '[' || brack == '{' {
+			stacBrack = append(stacBrack, brack)
+		} else if n == 0 {
+			return false
+		} else {
+
+			if (brack == ')' && stacBrack[n-1] == '(') || (brack == ']' && stacBrack[n-1] == '[') || (brack == '}' && stacBrack[n-1] == '{') {
+				stacBrack = append(stacBrack[:len(stacBrack)-1], stacBrack[len(stacBrack):]...)
+			} else {
+				return false
+			}
+		}
+	}
+	return len(stacBrack) == 0
+}
